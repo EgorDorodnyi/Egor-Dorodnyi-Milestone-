eb3977949fecaaa2e4bef971ae097d57bb442a1f
EgorDorodnyi <dorodnyie@wsc.school.nz>
2025-05-22 11:09:16 +1200

    Create Milestone_2.py

---
+++ Milestone 2/Milestone_2.py
@@ -0,0 +1,12 @@
+Strong_length = 8
+Letters_lowercase = "abcdefghijklmnopqrstuvwxyz"
+Letters_uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+print("welcome to the password checker")
+print("We do not store your password or save it anywhere. as well we do not store any of your data.")
+Password = input("Please enter your password: ")
+
+elif Password >= len(Strong_length):
+
+
+
