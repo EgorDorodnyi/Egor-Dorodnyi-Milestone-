1b432a37b1081a65646ef9ff1996a0b4dfb90229
EgorDorodnyi <dorodnyie@wsc.school.nz>
2025-05-27 13:42:41 +1200

    m

--- Milestone 3/Milestone_3.py
+++ Milestone 3/Milestone_3.py
@@ -1,8 +1,62 @@
 #storing anser and questions
+#if bum == Right_answer[0]:
+score = 0
+Right_answer = ["A", "D", "C", "B", "A"]
+Questions = ["what does Italy make the most of?", "what contory has the oldest University?", "when was  University of Bologna founded",
+              "what did Itay event?", "how old is the Roman Colosseum?"]
 
-My_list = ["ballz", "choped" , "jizz"]
-print(My_list)
-  
-if ballz in My_list:
-    print("psbolaz")
-  
+print("welcome to my Italy quiz\nInput A, B, C, D for it to regster as the right answer!")
+
+answer1 = input(" {}\nA: Wine\nB: Chezz\nC: pasta\nD: mac and chezz\n:".format(Questions[0])).upper()
+
+if answer1 == Right_answer[0]:
+    print("you got it right, Good boyyyyy :)")
+    score += 1
+else:
+    print(" Thats wrong!\n Italy is the largist producer of wine in the World")
+
+
+answer2 = input(" {}\nA: Brazil\nB: Usa\nC: UK\nD: Italy\n:".format(Questions[1])).upper()
+
+if answer2 == Right_answer[1]:
+    print("Amazing, you got it right!")
+    score += 1
+else:
+    print(' Thats wrong!\n Italy has the oldest University called "University of Bologna"')
+
+
+answer3 = input(" {}\nA: 839\nB: 1753\nC: 1088\nD: 1811\n:".format(Questions[2])).upper
+
+if answer3 == Right_answer[2]:
+    print("Your about to melt this quiz! Good job!!!!!")
+    score += 1
+else:
+    print(" Thats wrong!\n University of Bologna was founded in 1088")
+
+
+answer4 = input(" {}\nA: fish and chips\nB: Eyeglasses\nC: tank\nD: Computer\n:".format(Questions[3])).upper()
+
+if answer4 == Right_answer[3]:
+    print("Your knowlage is out of this world!!")
+    score += 1
+else:
+    print(" Thats wrong!\n Italy invented the eyeglasses!")
+
+
+answer5 = input(" {}\nA: 2000\nB: 500\nC: 1342\nD: 6969\n:".format(Questions[4])).upper()
+
+if answer5 == Right_answer[4]:
+    print("Thats right!! Your on fire!")
+    score += 1
+else:
+    print(" Thats wrong!\n The Roaman colosseum was made 2000 years ago!")
+print(score)
+
+
+
+
+
+
+
+
+
